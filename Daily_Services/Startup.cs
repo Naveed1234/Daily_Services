647ab1e6e8a0ca0c22fd771e1d90f6892b1d03fe
Naveed1234 <32640837+Naveed1234@users.noreply.github.com>
2019-04-29 17:56:55 +0500

    Add project files.

---
+++ Daily_Services/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Daily_Services.Startup))]
+namespace Daily_Services
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
